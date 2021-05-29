cbeace54b3a127b41faa03531195b5ae220c10e0
Soraturtle09 <lenny.jakobsen@gmx.net>
2021-05-29 11:15:53 +0200

    fixed the button
    er wechselt jetzt die Farbe

--- docs/script.js
+++ docs/script.js
@@ -66,3 +66,10 @@ document.addEventListener("DOMContentLoaded", function (event) {
 // OTHER SCRIPTS //
 ///////////////////
 
+function randomColor() {
+    var color = '#' + Math.random().toString(16).substr(2, 6);
+    document.getElementById("basicButton").style.backgroundColor = color;
+  
+    document.getElementById("basicButton").innerHTML = color;
+    console.log(color);
+  }
